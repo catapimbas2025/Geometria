7566f69f25134c54d59a052f31bc765e17ef944f
catapimbas2025 <catapimbasdoti@gmail.com>
2025-04-09 16:49:37 -0300

    ALTERAÇÕES...

--- Geometria/Plana/Contracts/ICircunferencia.cs
+++ Geometria/Plana/Contracts/ICircunferencia.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//CREATE BY PEDRO 
 namespace Geometria.Plana.Contracts
 {
     public interface ICircunferencia
